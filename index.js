340e01bf171faa3dc0495d3ac8b9f8fd867ba845
Praveen-BE <praveenrajendiran3939@gmail.com>
2025-01-28 07:35:15 +0530

    vercel config

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from "express";
 import restaurantListRouter from "./routes/restaurantListAPI.js";
 import restaurantMenuRouter from "./routes/restaurantMenuAPI.js";
 import cors from "cors";
-// Backend Local host
-const PORT = 3333;
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3333;
 const app = express();
 
-//frontend local host
 app.use(cors({ origin: "http://localhost:1234", credentials: true }));
 
 app.use(express.json());
